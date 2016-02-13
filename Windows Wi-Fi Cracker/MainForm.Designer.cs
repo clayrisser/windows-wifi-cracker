3ce50a6b7b3188dcea50ca2f915b4b6fc542f136
jamrizzi <jamrizzi@gmail.com>
2016-02-13 16:41:08 -0500

    Main functionality working

--- Windows Wi-Fi Cracker/MainForm.Designer.cs
+++ Windows Wi-Fi Cracker/MainForm.Designer.cs
@@ -24,50 +24,85 @@
         /// </summary>
         private void InitializeComponent() {
             this.response = new System.Windows.Forms.Label();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.mainTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.profilesTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.mainTableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // response
             // 
             this.response.AutoSize = true;
+            this.response.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.response.Location = new System.Drawing.Point(3, 0);
             this.response.Name = "response";
-            this.response.Size = new System.Drawing.Size(55, 13);
+            this.response.Size = new System.Drawing.Size(207, 24);
             this.response.TabIndex = 1;
-            this.response.Text = "Response";
+            this.response.Text = "Windows Wi-Fi Cracker";
             // 
-            // tableLayoutPanel1
+            // mainTableLayoutPanel
             // 
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.response, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 261);
-            this.tableLayoutPanel1.TabIndex = 2;
+            this.mainTableLayoutPanel.ColumnCount = 2;
+            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.mainTableLayoutPanel.Controls.Add(this.response, 0, 0);
+            this.mainTableLayoutPanel.Controls.Add(this.profilesTableLayoutPanel, 0, 1);
+            this.mainTableLayoutPanel.Controls.Add(this.exportButton, 1, 1);
+            this.mainTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainTableLayoutPanel.Name = "mainTableLayoutPanel";
+            this.mainTableLayoutPanel.RowCount = 2;
+            this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.mainTableLayoutPanel.Size = new System.Drawing.Size(553, 332);
+            this.mainTableLayoutPanel.TabIndex = 2;
             // 
-            // Form1
+            // profilesTableLayoutPanel
+            // 
+            this.profilesTableLayoutPanel.AutoScroll = true;
+            this.profilesTableLayoutPanel.ColumnCount = 3;
+            this.profilesTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.profilesTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.profilesTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.profilesTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.profilesTableLayoutPanel.Location = new System.Drawing.Point(3, 73);
+            this.profilesTableLayoutPanel.Name = "profilesTableLayoutPanel";
+            this.profilesTableLayoutPanel.RowCount = 1;
+            this.profilesTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.profilesTableLayoutPanel.Size = new System.Drawing.Size(380, 256);
+            this.profilesTableLayoutPanel.TabIndex = 2;
+            // 
+            // exportButton
+            // 
+            this.exportButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.exportButton.Location = new System.Drawing.Point(432, 306);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(75, 23);
+            this.exportButton.TabIndex = 3;
+            this.exportButton.Text = "Export";
+            this.exportButton.UseVisualStyleBackColor = true;
+            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(553, 332);
+            this.Controls.Add(this.mainTableLayoutPanel);
+            this.Name = "MainForm";
             this.Text = "Windows Wi-Fi Cracker";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
+            this.mainTableLayoutPanel.ResumeLayout(false);
+            this.mainTableLayoutPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.Label response;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel mainTableLayoutPanel;
+        private System.Windows.Forms.TableLayoutPanel profilesTableLayoutPanel;
+        private System.Windows.Forms.Button exportButton;
     }
 }
 
